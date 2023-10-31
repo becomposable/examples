380c4cf8896331ff3240f7ff25fb3d6fed0718ad
Eric Barroca <eric@vintners.co>
2023-10-31 14:16:15 +0900

    better exmaple + doc

--- story-generator-react/README.md
+++ story-generator-react/README.md
@@ -1,27 +1,34 @@
-# React + TypeScript + Vite
+# Composable Prompt's SDK + React
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Quickly create Generative AI-powered React Apps.
+Uses React + Vite.
 
-Currently, two official plugins are available:
+## Start the Example
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+### Setup Composable Prompts
 
-## Expanding the ESLint configuration
+Get an account on [Composable Prompts](https://composableprompts.com/) (free to try).
+Create a new project and copy the API key.
 
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
+Then
 
-- Configure the top-level `parserOptions` property like this:
-
-```js
-   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-   },
+```bash
+export COMPOSABLE_PROMPTS_API_KEY=<your api key>
 ```
 
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+### Launch the example
+
+```bash
+#at the repository root
+$ npm install
+
+#With Bun
+$ bun run dev
+
+#With Vite
+$ vite dev
+```
+
+Now you can connect to http://localhost:5174/ and clic on Generate Story.
+It will execute the interaction.
+You can experiment with various parameters to see how to configure the Interaction generating the story.
--- story-generator-react/src/App.tsx
+++ story-generator-react/src/App.tsx
@@ -1,52 +1,99 @@
-import { useCallback, useState } from "react";
-import { GenerateAStory, configure } from "./interaction"
-import { ExecutionRun } from "@composableai/studio";
+import { ExecutionRun } from '@composableai/studio';
+import { useCallback, useRef, useState } from 'react';
+import { GenerateAStory, GenerateAStoryProps, configure } from './interaction';
 
 configure({
-	apikey: "sk-ec54686e78643101d7133b95ea2c43c5"
-})
+    apikey: 'sk-ec54686e78643101d7133b95ea2c43c5',
+});
 
 function App() {
-	const [run, setRun] = useState<ExecutionRun>();
-	const [content, setContent] = useState<string>();
+    const [run, setRun] = useState<ExecutionRun>();
+    const [content, setContent] = useState<string>();
+    const [data, setData] = useState<GenerateAStoryProps>({
+        student_name: 'Julien',
+        student_age: 12,
+        interests: ['piano', 'anime', 'video games'],
+        study_language: 'French',
+        user_language: 'English',
+        type: 'morning story',
+        topic: '',
+        level: 'intermediate',
+        length: 300,
+        style: 'Alexandre Dumas',
+    });
+    const dataRef = useRef<HTMLTextAreaElement>(null);
+    const [isGenerating, setIsGenerating] = useState<boolean>(false);
 
-	const onClick = useCallback(() => {
-		const chunks: string[] = [];
-		new GenerateAStory().execute({
-			data: {
-				"student_name": "Julien",
-				"student_age": 12,
-				"interests": [
-					"piano",
-					"anime",
-					"video games"
-				],
-				"study_language": "French",
-				"user_language": "English",
-				"type": "morning story",
-				"topic": "",
-				"level": "intermediate",
-				"length": 300,
-				"style": "Alexandre Dumas"
-			}
-		}, (run) => {
-			setRun(run);
-		}, (chunk: string) => {
-			chunks.push(chunk);
-			setContent(chunks.join(''));
-		})
-	}, []);
+    const onClick = useCallback(() => {
+        const content = dataRef.current ? dataRef.current.value : '';
+        if (content == null) {
+            alert('No data');
+            return;
+        }
+        try {
+            const json = JSON.parse(content);
+            setData(json);
+        } catch (e) {
+            alert('Invalid JSON: ' + e);
+            return;
+        }
 
-	return (
-		<div>
-			<button onClick={onClick}>Generate story</button>
-			<div style={{ marginTop: '10px', whiteSpace: "pre-wrap" }}>
-				{
-					run ? run.result : <span className={content == null ? '' : 'chunks'}>{content || ''}</span>
-				}
-			</div>
-		</div>
-	)
+        setIsGenerating(true);
+        const chunks: string[] = [];
+        new GenerateAStory()
+            .execute(
+                { data: data },
+                (run) => {
+                    setRun(run);
+                },
+                (chunk: string) => {
+                    chunks.push(chunk);
+                    setContent(chunks.join(''));
+                }
+            )
+            .finally(() => {
+                setIsGenerating(false);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Generate a Story - Composable Prompts + React</h1>
+
+            <div style={{ marginTop: '10px', flex: 'flex: 1', flexDirection: 'row' }}>
+                <div style={{ flexDirection: 'column' }}>
+                    <h2>
+                        Input Data (<code>StoryGeneratorProps</code>)
+                    </h2>
+                    <textarea
+                        style={{ width: '100%', height: '200px' }}
+                        defaultValue={JSON.stringify(data, null, 2)}
+                        ref={dataRef}
+                    />
+                    <button onClick={onClick} disabled={isGenerating}>
+                        {isGenerating ? 'Generating...' : 'Generate story'}
+                    </button>
+                </div>
+                <div style={{ flexDirection: 'column' }}>
+                    <h2>Story</h2>
+                    <div
+                        style={{
+                            marginTop: '10px',
+                            whiteSpace: 'pre-wrap',
+                            border: '1px gray solid',
+                            padding: '4px',
+                        }}
+                    >
+                        {run ? (
+                            run.result
+                        ) : (
+                            <span className={content == null ? '' : 'chunks'}>{content || ''}</span>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
