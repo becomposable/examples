4ddebe4c37cc983a5be6b845931aeaa5c389ef14
Mincong Huang <mhuang@becomposable.com>
2024-10-31 07:17:25 +0100

    Replace git-diff by git-log to include authors

--- api-doc-gen/recipes/release-notes.ts
+++ api-doc-gen/recipes/release-notes.ts
@@ -8,7 +8,7 @@ if (!start || !end) {
 
 const cwd = tmpdir();
 
-console.log(`Retrieving issues between ${start}and ${end}...`)
+console.log(`Retrieving issues between ${start} and ${end}...`)
 // Get list of commit logs containing '#' between the two tags and extract unique issue numbers
 const issue_numbers = await exec(`git log ${start}..${end} --oneline | grep -o '#[0-9]\\+' | sed 's/#//' | sort -u`) as string;
 
@@ -19,7 +19,9 @@ for (const issue of issue_numbers.trim().split("\n")) {
 }
 
 console.log("Generating diff");
-await exec(`git diff --submodule=diff ${start}...${end} > ${cwd}/range_diff.txt`)
+// We use git-log to get authors, commit messages and code diff.
+// We use --submodule=diff to include submodule changes
+await exec(`git log --submodule=diff --patch ${start}...${end} > ${cwd}/range_diff.txt`)
 copy(`${cwd}/range_diff.txt`, "range_diff.txt");
 
 export default {
