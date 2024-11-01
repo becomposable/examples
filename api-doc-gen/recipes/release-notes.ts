d59dcfa9b92321ec45954df067fdfd092d0e425a
Mincong Huang <mincong.h@gmail.com>
2024-11-01 18:38:06 +0100

    feat: improve data collection for release notes generation (#6)
    * Support patterns like fix(123) and feat(123)
    
    * Make commands compatible with macOS
    
    * Remove parentheses
    
    * List issues of a milestone_name

--- api-doc-gen/recipes/release-notes.ts
+++ api-doc-gen/recipes/release-notes.ts
@@ -1,6 +1,6 @@
 import { copy, copyText, exec, tmpdir, vars } from "@becomposable/memory-commands";
 
-const { start, end } = vars();
+const { start, end, milestone } = vars();
 if (!start || !end) {
     console.error("Please provide start and end tags using --var-start and --var-end");
     process.exit(1);
@@ -9,10 +9,25 @@ if (!start || !end) {
 const cwd = tmpdir();
 
 console.log(`Retrieving issues between ${start} and ${end}...`)
-// Get list of commit logs containing '#' between the two tags and extract unique reference IDs (issue or pull-request)
-const referenceIds = await exec(`git log ${start}..${end} --oneline | grep -o '#[0-9]\\+' | sed 's/#//' | sort -u`) as string;
 
-for (const reference of referenceIds.trim().split("\n")) {
+// Get GitHub reference IDs (issue or pull-request) from commit messages
+// Commits having the hashtag, like "Fix #123" => 123
+const hashtagIds = await exec(`git log ${start}..${end} --oneline | grep -o -E '#[0-9]+' | sed 's/#//'`) as string;
+// Commits with patterns like "fix(456) ..." or "feat(456) ..." => 456
+const patternIds = await exec(`git log ${start}..${end} --oneline | grep -o -E '\\([0-9]+\\)' | sed 's/[()]//g'`) as string;
+
+const issueIds = new Set<string>();
+if (milestone) {
+    console.log(`Retrieving issues for milestone ${milestone}...`)
+    const content = await exec(`gh issue list --milestone "${milestone}" --state all --json number --jq '.[].number'`) as string;
+    content.trim().split("\n").forEach(v => issueIds.add(v));
+}
+
+const unsortedReferenceIds = new Set(`${hashtagIds}\n${patternIds}`.trim().split("\n").map(v => v.trim()).map(Number));
+const referenceIds = Array.from(unsortedReferenceIds).sort((a, b) => a - b);
+console.log(`Found ${referenceIds.length} references`);
+
+for (const reference of referenceIds) {
     console.log(`Processing reference #${reference}`)
     let content = await exec(`gh pr view ${reference}`) as string;
     if (content) {
@@ -21,7 +36,14 @@ for (const reference of referenceIds.trim().split("\n")) {
         console.debug(`Failed to get content as pull-request for #${reference}, trying as issue`);
         content = await exec(`gh issue view ${reference}`) as string;
         copyText(content, `issues/${reference}.txt`);
+        issueIds.delete(`${reference}`);
     }
+}
+
+for (const issue of issueIds) {
+    console.log(`Processing reference #${issue}`)
+    let content = await exec(`gh issue view ${issue}`) as string;
+    copyText(content, `issues/${issue}.txt`);
 }
 
 console.log("Generating diff");
